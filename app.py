55d71b4907a0a36f1325ea2e3bc676ae3603abf1
iammiguelsolis <miguelsoliscunza24@gmail.com>
2025-11-29 00:11:01 -0500

    primeros pasos

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+usuarios_db = [
+    {"id": 1, "nombre": "Miguel", "rol": "Admin"},
+    {"id": 2, "nombre": "Ana", "rol": "Cliente"},
+    {"id": 3, "nombre": "Carlos", "rol": "Vendedor"}
+]
 
+@app.route('/')
 def home():
-    return "Hello, World!"
+    return "Servidor funcionando. Ve a /usuarios para ver la magia."
+ 
+@app.route('/usuarios') 
+def get_usuarios():
+    return jsonify(usuarios_db)
+
+@app.route('/usuarios/<int:id>')
+def usuario_por_id(id):
+
+    usuario = None
+    
+    for u in usuarios_db:
+        if u['id'] == id:
+            usuario = u
+            break
+    if usuario:
+        return jsonify(usuario)
+    else:
+        return jsonify({"mensaje": "Usuario no encontrado"}), 404
   
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
